d104eb915353cccb7be0358e8aca4283c58e6132
Evan-Minor <42878572+Evan-Minor@users.noreply.github.com>
2020-04-03 12:08:55 -0600

    bug fixes

--- app/src/main/java/com/example/squaretap/home.java
+++ app/src/main/java/com/example/squaretap/home.java
@@ -112,7 +112,7 @@ public class home extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+     //   findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         Button btn = (Button)findViewById(R.id.btnTrigger);
 
